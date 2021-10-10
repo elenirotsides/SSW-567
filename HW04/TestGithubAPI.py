1f8c079d08875e2142718cc153228e2ed7fe8d0d
Eleni Rotsides <43190242+elenirotsides@users.noreply.github.com>
2021-10-09 23:56:00 -0400

    HW05a

--- HW04/TestGithubAPI.py
+++ HW04/TestGithubAPI.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import patch,  MagicMock
 
 from githubAPI import user_info
 
@@ -6,8 +7,18 @@ from githubAPI import user_info
 class TestGithubAPI(unittest.TestCase):
 
     def test_num_of_repos(self):
-        numOfRepos = len(user_info('elenirotsides'))
-        self.assertEqual(numOfRepos, 16)
+
+        m1 = MagicMock()
+        m1.return_value.json = [{'name': 'Repo1'}, {
+            'name': 'Repo2'}, {'name': 'A-Really-Cool-Repo'}, {'name': 'Repo4'}, {'name': 'Repo5'}]
+
+        m2 = MagicMock()
+        m2.return_value.json = [{'blah': 'blah'}, {'blah': 'blah'}, {
+            'blah': 'blah'}, {'blah': 'blah'}, {'blah': 'blah'}]
+
+        with patch('requests.get', return_value=m1):
+            with patch('requests.get', return_value=m2):
+                self.assertEqual(len(user_info('elenirotsides')), 5)
 
     def test_commit_count(self):
         info = user_info('elenirotsides')
